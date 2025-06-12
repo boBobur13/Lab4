36432a7598eaf2ab1749cc73f55a3438c83a04f3
Brain17 <b.daminov@newuu.uz>
2025-06-12 15:52:38 +0500

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=0;
+    cout << n<<endl;
+    for(char i='65';i<='90';i++){
+        cout <<i<< " " ;
+        
+         if (i % 5 == 0 {
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
